198534ff39329b660963f8c61d92e0fb2824fa5a
lkendi <lkendi003@gmail.com>
2024-10-17 15:59:52 +0300

    Code cleanup

--- WeatherApp.Infrastructure/Settings/WeatherApiSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace WeatherApp.Infrastructure.Settings
-{
-    public class WeatherApiSettings
-    {
-        public string ApiKey { get; set; } = string.Empty;
-        public string BaseUrl { get; set; } = string.Empty;
-    }
-}
